29f296615ea42f5239c58df9e6b7731a9edd0379
Dariusz D <darekduda21@gmail.com>
2024-04-12 22:32:34 -0400

    Export rest of plugins

---
+++ lua/kickstart/plugins/conform.lua
@@ -0,0 +1,42 @@
+return {
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- You can use a function here to determine the formatters dynamically
+        python = function(bufnr)
+          if require('conform').get_formatter_info('ruff_format', bufnr).available then
+            return { 'ruff_format' }
+          else
+            return { 'isort', 'black' }
+          end
+        end,
+        -- Conform will run multiple formatters sequentially
+        go = { 'goimports', 'gofmt' },
+      },
+    },
+  },
+}
---
+++ lua/kickstart/plugins/theme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    -- Theme with pastele colors
+    'catppuccin/nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
+  },
+}
