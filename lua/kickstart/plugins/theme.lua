0e883238549d9365bfbd8e3a4d24ebfdbae973ad
Dariusz D <darekduda21@gmail.com>
2024-04-19 21:14:57 -0400

    Load theme correctly with transparent background

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -4,6 +4,9 @@ return {
     'catppuccin/nvim',
     priority = 1000,
     config = function()
+      require('catppuccin').setup {
+        transparent_background = true,
+      }
       vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
