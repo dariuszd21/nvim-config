46180d44ce8988ba4ed8a0245006804c04c6671a
Dariusz D <darekduda21@gmail.com>
2024-08-30 21:40:07 -0400

    feat: add inline variable expanding in debug
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -14,10 +14,13 @@ return {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
 
-    -- Required dependency for nvim-dap-ui
+    -- Expand variables inline
+    'theHamsta/nvim-dap-virtual-text',
+
+    -- Required dependency for nvim-dap-ui and expanding virtual text
     'nvim-neotest/nvim-nio',
 
-    -- Add your own debuggers here
+    -- Per language specific plugins
     'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
   },
@@ -30,6 +33,7 @@ return {
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>db', dap.run_to_cursor, { desc = 'Debug: Run to cursor' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -60,9 +64,17 @@ return {
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
 
+    -- Display variable under cursor
+    vim.keymap.set('n', '<space>=', function()
+      require('dapui').eval(nil, { enter = true })
+    end)
+
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    -- Initialize virtual text plugin
+    require('nvim-dap-virtual-text').setup {}
 
     -- Install golang specific config
     require('dap-go').setup()
