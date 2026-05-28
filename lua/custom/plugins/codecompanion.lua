edf74643f2ac6256768aee587e6085ab1e290d7e
Dariusz D <9116056+dariuszd21@users.noreply.github.com>
2026-05-28 13:37:53 +0200

    fix(codecompanion): correctly lazy load on keybind press

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -6,28 +6,28 @@ return {
   },
   version = '^18.0.0',
 
+  opts = {},
   -- Only load when these commands are invoked
   cmd = {
     'CodeCompanion',
     'CodeCompanionChat',
-    'CodeCompanionExplain',
     'CodeCompanionActions',
   },
   keys = {
     {
+      '<leader>aa',
+      '<cmd>CodeCompanionActions<cr>',
+      desc = '[A]I [A]ctions (CodeCompanion)',
+    },
+    {
       '<leader>ac',
-      function()
-        vim.cmd 'CodeCompanionChat'
-      end,
+      '<cmd>CodeCompanionChat<cr>',
       desc = '[A]I [C]hat (CodeCompanion)',
     },
     {
-      '<leader>ae',
-      function()
-        vim.cmd 'CodeCompanion Explain'
-      end,
-      mode = 'v',
-      desc = '[A]I [E]xplain selected code',
+      '<leader>ap',
+      '<cmd>CodeCompanion<cr>',
+      desc = '[A]I [P]rompt (CodeCompanion)',
     },
   },
 
